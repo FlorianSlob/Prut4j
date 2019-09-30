480b2c30b037ac5d42631459fa5fc6eb3db97ecd
Florian Slob <F.Slob@certusportautomation.com>
2019-09-30 23:38:40 +0200

    Created project with unit test for pipeline.

---
+++ model-checking-sandbox/src/main/java/nl/florianslob/model/checking/sandbox/App.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.florianslob.model.checking.sandbox;
+
+/**
+ *
+ * @author FlorianSlob
+ */
+public class App {
+    public static void Main(String[] args){
+        System.out.println("nl.florianslob.model.checking.sandbox.App.Main()");
+    }
+}
---
+++ model-checking-sandbox/src/test/java/nl/florianslob/model/checking/sandbox/tests/AppTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.florianslob.model.checking.sandbox.tests;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
