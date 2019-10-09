51b4a226c64ef0831e4fcca4c9b4061dedfe067d
Florian Slob <florianslob@gmail.com>
2019-10-09 21:47:04 +0200

    Added some files to gitignore & made project runnable by adding main method (small letter 'm')

---
+++ src/main/java/nl/florianslob/model/checking/sandbox/Main.java
@@ -0,0 +1,11 @@
+package nl.florianslob.model.checking.sandbox;
+
+/**
+ *
+ * @author FlorianSlob
+ */
+public class Main {
+    public static void main(String[] args){
+       System.out.println("nl.florianslob.model.checking.sandbox.Main.Main()");
+    }    
+}
