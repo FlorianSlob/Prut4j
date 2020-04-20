7ee1b24a604b2013eef2e0f6a8ed498b471b0a50
Florian Slob <florianslob@gmail.com>
2020-04-20 07:34:19 +0200

    Added project to test generated code

---
+++ model-checking-testproject/src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("This project will contain logic that uses the generated code.");
+    }
+}
