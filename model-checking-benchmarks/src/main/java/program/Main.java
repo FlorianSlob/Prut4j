94e0e31c62c1bcfa7868e1620882ffc5ae8312fc
Florian Slob <florianslob@gmail.com>
2020-10-20 18:06:45 +0200

    Changed some log lines and cleaned up a bit

--- model-checking-benchmarks/src/main/java/program/Main.java
+++ model-checking-benchmarks/src/main/java/program/Main.java
@@ -3,15 +3,15 @@ package program;
 public class Main {
     public static void main(String[] args) {
         // Override args for testing purposes.
-        var BTArs = new String[]{"class=W", "-np22"};
+        var BTArgs = new String[]{"class=S", "-np2"};
 
         // execute a couple of times
         for(int i = 0; i < 1; i++){
             System.out.println("Executing original benchmark.");
-            NPB3_0_JAV.BT.main(BTArs);
+            NPB3_0_JAV.BT.main(BTArgs);
 
             System.out.println("Executing update benchmark.");
-            NPB.updated.NPB3_0_JAV.BT.main(BTArs);
+            NPB.updated.NPB3_0_JAV.BT.main(BTArgs);
         }
 
     }
