d63737feed531a3d173c3c144b4f02b4b84a87d4
Florian Slob <florianslob@gmail.com>
2020-10-20 19:43:43 +0200

    Fixed import

--- model-checking-benchmarks/src/main/java/program/Main.java
+++ model-checking-benchmarks/src/main/java/program/Main.java
@@ -5,14 +5,10 @@ public class Main {
         // Override args for testing purposes.
         var BTArgs = new String[]{"class=S", "-np2"};
 
-        // execute a couple of times
-        for(int i = 0; i < 1; i++){
-            System.out.println("Executing original benchmark.");
-            NPB3_0_JAV.BT.main(BTArgs);
+        System.out.println("Executing original benchmark.");
+        NPB.original.NPB3_0_JAV.BT.main(BTArgs);
 
-            System.out.println("Executing update benchmark.");
-            NPB.updated.NPB3_0_JAV.BT.main(BTArgs);
-        }
-
+        System.out.println("Executing update benchmark.");
+        NPB.updated.NPB3_0_JAV.BT.main(BTArgs);
     }
 }
