b1a3564bdebfc0e116d2d42780c0710cef963321
Florian Slob <florianslob@gmail.com>
2020-11-02 16:49:12 +0100

    Removed duplicated discourje files and added jar reference

--- model-checking-benchmarks/src/main/java/program/Main.java
+++ model-checking-benchmarks/src/main/java/program/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Override args for testing purposes.
         // Problem classes to choose from: S,W,A,B,C,D,E,F
-        var BTArgs = new String[]{"class=C", "-np1"};
+        var BTArgs = new String[]{"class=B", "-np1"};
 
         System.out.println("Executing original benchmark.");
         NPB.original.NPB3_0_JAV.MG.main(BTArgs);
