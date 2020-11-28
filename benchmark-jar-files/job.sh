0b34a69eb5038d07573d6f5e8200266ab2c1e577
Sung-Shik Jongmans <ssj@ou.nl>
2020-11-28 15:01:35 +0100

    Added scripts to run benchmarks at Cartesius

---
+++ benchmark-jar-files/job.sh
@@ -0,0 +1,23 @@
+#SBATCH -N 1
+#SBATCH -p normal
+#SBATCH -o out.%A
+#SBATCH -t 24:00:00
+
+JAR=model-checking-benchmarks-v0.1.jar
+MODE=$1 # "Original" or "Updated"
+PROGRAM=$2
+ITERS=$3
+
+echo "MODE=$1, PROGRAM=$2, ITERS=$3" 
+echo
+
+hostname
+cat /proc/cpuinfo
+
+echo
+
+for K in $(seq 2 2 24); do
+    for I in $(seq 1 $ITERS); do
+        java -jar $JAR -run"$MODE"Benchmark -BM=$PROGRAM -np$K class=C
+    done
+done
---
+++ benchmark-jar-files/sbatch.sh
@@ -0,0 +1,10 @@
+
+sbatch job.sh Original CG 30
+sbatch job.sh Original FT 30
+sbatch job.sh Original IS 30
+sbatch job.sh Original MG 30
+
+sbatch job.sh Updated CG 30
+sbatch job.sh Updated FT 30
+sbatch job.sh Updated IS 30
+sbatch job.sh Updated MG 30
