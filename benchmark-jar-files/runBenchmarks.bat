9a2fcda08ae4196c2e081a2ab5763925ad5737b6
Florian Slob <florianslob@gmail.com>
2020-11-30 19:24:01 +0100

    Updated Jar

--- benchmark-jar-files/runBenchmarks.bat
+++ benchmark-jar-files/runBenchmarks.bat
@@ -1,14 +1,15 @@
-
+	java -jar model-checking-benchmarks-v0.1.jar -runOriginalBenchmark -BM=CG -np24 class=B
+	java -jar model-checking-benchmarks-v0.1.jar -runUpdatedBenchmark  -BM=CG-alt -np24 class=B
 @echo off
 >output.txt (
-	java -jar model-checking-benchmarks-v0.1.jar -runOriginalBenchmark -BM=CG -np6 class=B
-	java -jar model-checking-benchmarks-v0.1.jar -runUpdatedBenchmark  -BM=CG-alt -np6 class=B
-	java -jar model-checking-benchmarks-v0.1.jar -runOriginalBenchmark -BM=CG -np6 class=B
-	java -jar model-checking-benchmarks-v0.1.jar -runUpdatedBenchmark  -BM=CG-alt -np6 class=B
-	java -jar model-checking-benchmarks-v0.1.jar -runOriginalBenchmark -BM=CG -np6 class=B
-	java -jar model-checking-benchmarks-v0.1.jar -runUpdatedBenchmark  -BM=CG-alt -np6 class=B
-	java -jar model-checking-benchmarks-v0.1.jar -runOriginalBenchmark -BM=CG -np6 class=B
-	java -jar model-checking-benchmarks-v0.1.jar -runUpdatedBenchmark  -BM=CG-alt -np6 class=B
-	java -jar model-checking-benchmarks-v0.1.jar -runOriginalBenchmark -BM=CG -np6 class=B
-	java -jar model-checking-benchmarks-v0.1.jar -runUpdatedBenchmark  -BM=CG-alt -np6 class=B
+	java -jar model-checking-benchmarks-v0.1.jar -runOriginalBenchmark -BM=CG -np24 class=B
+	java -jar model-checking-benchmarks-v0.1.jar -runUpdatedBenchmark  -BM=CG-alt -np24 class=B
+	java -jar model-checking-benchmarks-v0.1.jar -runOriginalBenchmark -BM=CG -np24 class=B
+	java -jar model-checking-benchmarks-v0.1.jar -runUpdatedBenchmark  -BM=CG-alt -np24 class=B
+	java -jar model-checking-benchmarks-v0.1.jar -runOriginalBenchmark -BM=CG -np24 class=B
+	java -jar model-checking-benchmarks-v0.1.jar -runUpdatedBenchmark  -BM=CG-alt -np24 class=B
+	java -jar model-checking-benchmarks-v0.1.jar -runOriginalBenchmark -BM=CG -np24 class=B
+	java -jar model-checking-benchmarks-v0.1.jar -runUpdatedBenchmark  -BM=CG-alt -np24 class=B
+	java -jar model-checking-benchmarks-v0.1.jar -runOriginalBenchmark -BM=CG -np24 class=B
+	java -jar model-checking-benchmarks-v0.1.jar -runUpdatedBenchmark  -BM=CG-alt -np24 class=B
 )
